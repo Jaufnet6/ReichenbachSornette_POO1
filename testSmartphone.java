3cc0c49ac3302cc0e5a96589d8f34b24a1006f38
Jaufray <jaufray.sornette@gmail.com>
2017-05-15 19:21:10 +0200

    solve error testSmartphone

--- testSmartphone.java
+++ testSmartphone.java
@@ -4,7 +4,7 @@ public class testSmartphone {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		GalleryFrame test = new GalleryFrame();
+		GalleryFrame test = new GalleryFrame();
 //		test.setVisible(true);
 //		
 		CalculatorFrame testCalc = new CalculatorFrame();
