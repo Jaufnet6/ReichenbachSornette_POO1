208378c796127a7a82ff646d225b074b1cc53a77
Jaufray <jaufray.sornette@gmail.com>
2017-05-15 11:18:43 +0200

    Error Corrected Contact

--- Contact.java
+++ Contact.java
@@ -2,103 +2,104 @@ import java.io.Serializable;
 
 public class Contact implements Serializable{
 
-	private int[] numbers;
-	private int counterForNumbers;
-	private String firstName;
-	private String lastName;
-	private String companyName;
-	private String email;
-	private String birthday;
-	private String note;
+    private int[] numbers;
+    private int number;
+    private int counterForNumbers;
+    private String firstName;
+    private String lastName;
+    private String companyName;
+    private String email;
+    private String birthday;
+    private String note;
 
-	public Contact(String firstName, String lastName){
-		this.firstName = firstName;
-		this.lastName = lastName;
-		counterForNumbers = 0;
-	}
+    public Contact(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        counterForNumbers = 0;
+    }
 
-	//Setters and getters
-	public void setNumber(int number){
-		this.number = number;
-	}
-	public int getNumber(){
-		return number;
-	}
-	public void setLastName(String lastName){
-		this.lastName = lastName;
-	}
-	public String getLastName(){
-		return lastName;
-	}
-	public void setFirstName(String firstName){
-		this.firstName = firstName;
-	}
-	public String getFirstName(){
-		return firstName;
-	}
-	public void setCompanyName(String companyName){
-		this.companyName = companyName;
-	}
-	public String getCompanyName(){
-		return companyName;
-	}
-	public void setEmail(String email){
-		this.email = email;
-	}
-	public String getEmail(){
-		return email;
-	}
-	public void setBirthday(String birthday){
-		this.birthday = birthday;
-	}
-	public String getBirthday(){
-		return birthday;
-	}
-	public void setNote(String note){
-		this.note = note;
-	}
-	public String getNote(){
-		return note;
-	}
+    //Setters and getters
+    public void setNumber(int number){
+        this.number = number;
+    }
+    public int getNumber(){
+        return number;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setCompanyName(String companyName){
+        this.companyName = companyName;
+    }
+    public String getCompanyName(){
+        return companyName;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setBirthday(String birthday){
+        this.birthday = birthday;
+    }
+    public String getBirthday(){
+        return birthday;
+    }
+    public void setNote(String note){
+        this.note = note;
+    }
+    public String getNote(){
+        return note;
+    }
 
-	//Adds a new number in the contact
-	public void addNewPhoneNumber(int newNumber){
-		counterForNumbers++;
-		private int[] beforeAdding = numbers;
-		private int[] afterAdding = new int[counterForNumbers];
+    //Adds a new number in the contact
+    public void addNewPhoneNumber(int newNumber){
+        counterForNumbers++;
+        int[] beforeAdding = numbers;
+        int[] afterAdding = new int[counterForNumbers];
 
-		for(int i = 0; i < beforeAdding.length(); i++){
-			afterAdding[i] = beforeAdding[i];
-		}
-		afterAdding[afterAdding.length()-1] = newNumber;
+        for(int i = 0; i < beforeAdding.length; i++){
+            afterAdding[i] = beforeAdding[i];
+        }
+        afterAdding[afterAdding.length-1] = newNumber;
 
-		numbers = afterAdding;
-	}
+        numbers = afterAdding;
+    }
 
-	//Removes a number in the contact
-	public void removePhoneNumber(int deletedNumber){
-		counterForNumbers--;
-		private int[] beforeAdding = numbers;
-		private int[] afterAdding = new int[counterForNumbers];
+    //Removes a number in the contact
+    public void removePhoneNumber(int deletedNumber){
+        counterForNumbers--;
+        int[] beforeAdding = numbers;
+        int[] afterAdding = new int[counterForNumbers];
 
-		//deletes the number in the array
-		for(int i = 0; i < beforeAdding.length(); i++){
-			if(beforeAdding[i] == deletedNumber)
-				beforeAdding = null;
-		}
+        //deletes the number in the array
+        for(int i = 0; i < beforeAdding.length; i++){
+            if(beforeAdding[i] == deletedNumber)
+                beforeAdding[i] = 0;
+        }
 
-		//copies the values of the old array in the new one.
-		for(int i = 0; i < beforeAdding.length(); i++){
-			for(int j = 0; j < afterAdding.length(); i++){
-				if(afterAdding[j] == null){
-					if(beforeAdding[i] != null)
-						afterAdding[j] = beforeAdding[i];
-				}
-			}
-		}
+        //copies the values of the old array in the new one.
+        for(int i = 0; i < beforeAdding.length; i++){
+            for(int j = 0; j < afterAdding.length; i++){
+                if(afterAdding[j] == 0){
+                    if(beforeAdding[i] != 0)
+                        afterAdding[j] = beforeAdding[i];
+                }
+            }
+        }
 
-		numbers = afterAdding;
-	}
+        numbers = afterAdding;
+    }
 
 }
 
